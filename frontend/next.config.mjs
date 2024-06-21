248c033e5c3fa32e1209efe8501998d903abdf29
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-06-21 18:50:47 +0530

    Solved useSearchParams error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   crossOrigin: "use-credentials",
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
--- frontend/src/app/auth/reset-password/page.tsx
+++ frontend/src/app/auth/reset-password/page.tsx
@@ -1,3 +1,79 @@
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import {
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import axios from "axios";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import React, { useState } from "react";
+
+// const ResetPassword = () => {
+//   const [newPassword, setNewPassword] = useState<string>();
+//   const searchParams = useSearchParams();
+//   const router = useRouter();
+//   axios.defaults.withCredentials = true;
+
+//   const onSubmit = async () => {
+//     try {
+//       const sendMail = await axios.post(
+//         process.env.NEXT_PUBLIC_BACKEND_URL +
+//           `/api/auth/forgot-password/reset-password/${searchParams.get(
+//             "token"
+//           )}`,
+//         {
+//           newPassword,
+//         },
+//         {
+//           headers: {
+//             "Access-Control-Allow-Origin": "*",
+//           },
+//         }
+//       );
+//       alert(sendMail.data?.message);
+//       router.push("/auth/login");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <>
+//       <CardHeader>
+//         <CardTitle>Wecome back</CardTitle>
+//         <CardDescription>Set your new password! </CardDescription>
+//       </CardHeader>
+//       <CardContent>
+//         <form>
+//           <div className="grid w-full items-center gap-4">
+//             <div className="flex flex-col space-y-1.5">
+//               <Label htmlFor="password">Password</Label>
+//               <Input
+//                 id="password"
+//                 placeholder="********"
+//                 type="password"
+//                 onChange={(e) =>
+//                   // @ts-ignore
+//                   setNewPassword(e.target.value)
+//                 }
+//               />
+//             </div>
+//           </div>
+//         </form>
+//       </CardContent>
+//       <CardFooter className="flex justify-between">
+//         <Button variant="outline">Cancel</Button>
+//         <Button onClick={onSubmit}>Submit</Button>
+//       </CardFooter>
+//     </>
+//   );
+// };
+
+// export default ResetPassword;
 "use client";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,22 +86,23 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useRouter, useSearchParams, useParams } from "next/navigation";
+import React, { useState } from "react";
 
 const ResetPassword = () => {
+  axios.defaults.withCredentials = true;
   const [newPassword, setNewPassword] = useState<string>();
   const searchParams = useSearchParams();
   const router = useRouter();
-  axios.defaults.withCredentials = true;
 
   const onSubmit = async () => {
+    console.log(searchParams, "SearchParams");
+    const params = new URLSearchParams(searchParams);
     try {
       const sendMail = await axios.post(
         process.env.NEXT_PUBLIC_BACKEND_URL +
-          `/api/auth/forgot-password/reset-password/${searchParams.get(
-            "token"
-          )}`,
+          // @ts-ignore
+          `/api/auth/forgot-password/reset-password/${params.get("token")}`,
         {
           newPassword,
         },
