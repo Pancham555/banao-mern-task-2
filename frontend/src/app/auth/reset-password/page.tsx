897238bfa466a5afa9bdbe105b43bc164bf7254a
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-06-21 19:01:53 +0530

    Project completed

--- frontend/src/app/auth/reset-password/page.tsx
+++ frontend/src/app/auth/reset-password/page.tsx
@@ -1,79 +1,3 @@
-// "use client";
-// import { Button } from "@/components/ui/button";
-// import {
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import axios from "axios";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import React, { useState } from "react";
-
-// const ResetPassword = () => {
-//   const [newPassword, setNewPassword] = useState<string>();
-//   const searchParams = useSearchParams();
-//   const router = useRouter();
-//   axios.defaults.withCredentials = true;
-
-//   const onSubmit = async () => {
-//     try {
-//       const sendMail = await axios.post(
-//         process.env.NEXT_PUBLIC_BACKEND_URL +
-//           `/api/auth/forgot-password/reset-password/${searchParams.get(
-//             "token"
-//           )}`,
-//         {
-//           newPassword,
-//         },
-//         {
-//           headers: {
-//             "Access-Control-Allow-Origin": "*",
-//           },
-//         }
-//       );
-//       alert(sendMail.data?.message);
-//       router.push("/auth/login");
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   return (
-//     <>
-//       <CardHeader>
-//         <CardTitle>Wecome back</CardTitle>
-//         <CardDescription>Set your new password! </CardDescription>
-//       </CardHeader>
-//       <CardContent>
-//         <form>
-//           <div className="grid w-full items-center gap-4">
-//             <div className="flex flex-col space-y-1.5">
-//               <Label htmlFor="password">Password</Label>
-//               <Input
-//                 id="password"
-//                 placeholder="********"
-//                 type="password"
-//                 onChange={(e) =>
-//                   // @ts-ignore
-//                   setNewPassword(e.target.value)
-//                 }
-//               />
-//             </div>
-//           </div>
-//         </form>
-//       </CardContent>
-//       <CardFooter className="flex justify-between">
-//         <Button variant="outline">Cancel</Button>
-//         <Button onClick={onSubmit}>Submit</Button>
-//       </CardFooter>
-//     </>
-//   );
-// };
-
-// export default ResetPassword;
 "use client";
 import { Button } from "@/components/ui/button";
 import {
@@ -86,7 +10,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
-import { useRouter, useSearchParams, useParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
 const ResetPassword = () => {
@@ -101,7 +25,6 @@ const ResetPassword = () => {
     try {
       const sendMail = await axios.post(
         process.env.NEXT_PUBLIC_BACKEND_URL +
-          // @ts-ignore
           `/api/auth/forgot-password/reset-password/${params.get("token")}`,
         {
           newPassword,
